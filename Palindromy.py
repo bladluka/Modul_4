058659f7333643e3ee7406da805e2ae312fb090c
Łukasz <blada.lukasz@gmail.com>
2021-02-01 15:56:03 +0100

    2st draf after comment

--- Palindromy.py
+++ Palindromy.py
@@ -1,7 +1,17 @@
-słowo = 'kajak'
-def czy_palindrom(słowo):
-    return słowo == słowo[::-1]
-if czy_palindrom(słowo):
-    print(f"Slowo {słowo} jest palindromem")
+tekst = 'A to kiwi zdziwi kota'
+import string
+
+
+def czy_palindrom(tekst):
+    nowy_tekst = ''
+    for znak in tekst:
+        if znak in string.ascii_letters:
+            nowy_tekst += znak
+        nowy_tekst = nowy_tekst.lower()
+    return nowy_tekst == nowy_tekst[::-1]
+
+
+if czy_palindrom(tekst):
+    print(f"Tekst {tekst} jest palindromem")
 else:
-    print(f"Slowo {słowo} nie jest palindromem")
+    print(f"Tekst {tekst} nie jest palindromem")
